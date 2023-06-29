a29596986ed566b192cb781ac53ee7b113b5941b
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-06-29 16:35:06 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,1 +1,16 @@
-sudo cd /var/www/html/
+#!/bin/bash
+
+# Navigate to the wp-admin directory
+cd /var/www/html/wp-admin
+
+# Remove previous files
+rm -rf *
+
+# Clone the Git repository containing the new files
+git clone <repository_url> .
+
+# Perform any additional setup or configuration steps
+# ...
+
+# Change ownership and permissions if needed
+chown -R www-data:www-data .
